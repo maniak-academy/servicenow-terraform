f09443935a3fcf2fed7682c4936a6a06aa2dfa7b
Sebastian Maniak <sebastian@maniak.io>
2024-03-07 22:40:53 -0500

    resourcename

--- main.tf
+++ main.tf
@@ -29,6 +29,6 @@ resource "azurerm_resource_group" "resourcename" {
 resource "azurerm_virtual_network" "vnetname" {
   name                = var.vnet_name
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.resourcename.location
+  resource_group_name = azurerm_resource_group.resourcename.name
 }
